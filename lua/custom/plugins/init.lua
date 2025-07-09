c43bc9db004845079f89a8f629ba3ccb50c4af71
icyclestick <icebebe111@gmail.com>
2025-07-09 15:56:04 +0800

    first config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,30 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      {
+        'echasnovski/mini.icons',
+        opts = {
+          view_options = {
+            show_hidden = true,
+            show_icons = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+            },
+          },
+        },
+      },
+    },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+}
