d53a7a5cffe3fee781118d4d98e3c7e0ac3ef080
icyclestick <aisaiahalidio@gmail.com>
2025-10-03 15:57:37 +0800

    tsserver vs ts_ls

--- init.lua
+++ init.lua
@@ -616,8 +616,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
         --
+        tsserver = {},
         cssls = {},
 
         lua_ls = {
